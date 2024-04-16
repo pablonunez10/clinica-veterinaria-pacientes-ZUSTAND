74ed2a227aac183637f55c57bd6e813e9c9daff6
pablonuñez10 <nunezbenitezpablo@gmail.com>
2024-04-16 18:15:25 -0400

    first-test-ZUSTAND

---
+++ src/components/PatientList.tsx
@@ -0,0 +1,33 @@
+import { usePatientStore } from "../store"
+import PatientDetails from "./PatientDetails"
+export default function PatientList() {
+    const patients = usePatientStore(state => state.patients)
+
+  return (
+    <div className="md:-1/2 lg:3/5 md:h-screen overflow-y-scroll">
+      {patients.length ? (
+        <>
+            <h2 className="font-black text-3xl text-center">Listado de Pacientes</h2>
+            <p className="text-xl mt-5 mb-10 text-center">Administra tus {''}
+                <span className="text-indigo-600 font-bold" >Pacientes y Citas</span>
+            </p>
+            {patients.map(patient => {
+                <PatientDetails
+                    key={patient.id}
+                    patient={patient}
+                />
+            })}
+        </>
+        
+      ): (
+        <>
+            <h2 className="font-black text-3xl text-center">No hay pacientes</h2>
+            <p className="text-xl mt-5 mb-10 text-center">
+                Comienza agregando pacientes {''}
+                <span className="text-indigo-600 font-bold">Y se agregaran a este lugar</span>
+            </p>
+        </>
+      )}
+    </div>
+  )
+}
