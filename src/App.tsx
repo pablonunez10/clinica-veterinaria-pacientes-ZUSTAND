5b8dd3992ce958fbf73a544c96459af30a9a638c
pablonuñez10 <nunezbenitezpablo@gmail.com>
2024-04-17 11:35:44 -0400

    react-toastify

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
+import {ToastContainer} from 'react-toastify'
 import PatientForm from "./components/PatientForm"
 import PatientList from "./components/PatientList"
-
+import 'react-toastify/dist/ReactToastify.css'
 function App() {
 
   return (
@@ -15,6 +16,7 @@ function App() {
         <PatientList />
       </div>
     </div>
+    <ToastContainer/>
     </>
   )
 }
